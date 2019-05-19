682e9683e65fe09abf9938f8fe72edfb9f3cf295
Bartosz Dabrowski/Janosik <bart.dabrowski.uk@gmail.com>
2019-05-19 13:24:54 +0100

    Bubble Sort - done - optimalization

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -5,21 +5,67 @@ import java.util.Random;
 public class App
 {
     public static int[] BubbleSort(int [] tab, int n)
+{
+    int tmp;
+    int counter =0;
+    while(n>2)
+    {
+        boolean t= false;
+        for(int i =0; i<n-1;i++)
+        {
+            if(tab[i]>tab[i+1])
+            {
+                t=true;
+                tmp = tab[i];
+                tab[i] = tab[i+1];
+                tab[i+1] = tmp;
+
+            }
+            counter++;
+        }
+        if(t != true)
+        {
+            break;
+        }
+
+
+        n--;
+    }
+
+    System.out.println(counter);
+    return tab;
+}
+    public static int[] BubbleSort1(int [] tab, int n)
     {
         int tmp;
-        while(n>0)
+        int counter =0;
+        while(n>2)
         {
+            boolean t = false;
             for(int i =0; i<n-1;i++)
             {
-                if(tab[i]>tab[i+1])
+                if(tab[i] != tab[i+1] || !(tab[i]>tab[i+1]))
                 {
-                    tmp = tab[i];
-                    tab[i]=tab[i+1];
-                    tab[i+1] = tmp;
+                    if(tab[i]>tab[i+1])
+                    {
+                        t=true;
+                        tmp = tab[i];
+                        tab[i] = tab[i+1];
+                        tab[i+1] = tmp;
+
+                    }
                 }
+
+                counter++;
+            }
+            if(t != true)
+            {
+                break;
             }
             n--;
         }
+
+        System.out.println(counter);
         return tab;
     }
 
@@ -27,9 +73,9 @@ public class App
     public static void main( String[] args )
     {
 
-        int n = 10000;
-        int max = 5000;
-        int min = -5000;
+        int n = 100;
+        int max = 50;
+        int min = -50;
         int [] tab = new int[n];
         Random r= new Random();
         long startTime = System.currentTimeMillis();
@@ -46,11 +92,12 @@ public class App
             System.out.println(tab[i]);
 
         }*/
-        long startTime1 = System.currentTimeMillis();
+        /*long startTime1 = System.currentTimeMillis();
         BubbleSort(tab, n);
         long endTime1 = System.currentTimeMillis();
-        long Time1 = endTime1-startTime1;
-        System.out.println("Czas sortowania tablicy(w sekundach): "+Time1/1000+" oraz w milisekundach "+Time1);
+        long Time1 = endTime1-startTime1;*/
+        BubbleSort1(tab,n);
+        //System.out.println("Czas sortowania tablicy(w sekundach): "+Time1/1000+" oraz w milisekundach "+Time1);
         for(int i = 0; i<n;i++)
         {
             System.out.println(tab[i]);
